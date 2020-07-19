bc56d859152794a5a14c32cdec7beb063c5f5f7c
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2020-07-19 19:34:37 +0300

    release  V.1

---
+++ app/src/main/java/com/exemple/android/messenger/MainActivity.java
@@ -0,0 +1,41 @@
+package com.exemple.android.messenger;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TabsAccessorAdapter tabsAccessorAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.mine_page_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Messenger");
+
+        viewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        tabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabsAccessorAdapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+}
